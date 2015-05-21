6785cece95a44fc1026f0688b652c693233b1817
3wolftech <3wolftechnology@gmail.com>
2015-05-22 00:13:21 +0300

    some comments

--- app/src/main/java/com/threewolftech/shiftplanner/Login.java
+++ app/src/main/java/com/threewolftech/shiftplanner/Login.java
@@ -2,15 +2,27 @@ package com.threewolftech.shiftplanner;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class Login extends Activity {
+
+    Animation animLoginLogo;
+    ImageView ivLoginLogo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //Init
+        ivLoginLogo = (ImageView)findViewById(R.id.ivLoginLogo);
+
+        //Load the animations
+        animLoginLogo = AnimationUtils.loadAnimation(getApplicationContext(),R.animator.login_logo_anim);
+
+        //Start animation
+        ivLoginLogo.startAnimation(animLoginLogo);
     }
 }
